fb0eb522fd7fcccfbd5c8d0017f420bbef0b4def
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2022-04-12 19:52:05 +0900

    Format .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    arrowParens: "avoid",
-    printWidth: 80,
-    semi: true,
-    singleQuote: true,
-    useTabs: false,
-    tabWidth: 2,
-    trailingComma: "all",
-    endOfLine: "lf",
+  arrowParens: 'avoid',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  endOfLine: 'lf',
 };
